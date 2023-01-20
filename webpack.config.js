6bd93f837ae74da334c97c80e4035d8b7d7b0627
Wesley Isaias <wesley_isaias@hotmail.com>
2023-01-20 08:48:20 -0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry : path.resolve(__dirname,'src', 'index.jsx'),
+    output: {
+        path: path.resolve(__dirname,'dist'),
+        filename: 'bundle.js'
+    },
+    resolve:{
+        extensions: ['.js', '.jsx'],
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'public'),
+      },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, 'public','index.html')
+        })
+    ],
+        module: {
+            rules:[
+                {
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                use: 'babel-loader',
+            }
+        ],
+    }
+};
