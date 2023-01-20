4afcac05561de98e6af45a61170d480e9d1dcb5c
Wesley Isaias <wesley_isaias@hotmail.com>
2023-01-20 11:49:04 -0300

    Iniciando 02-Conceitos Importantes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
+import { RepositoryList } from './components/RepositoryList';
 import './styles/global.scss';
 
 export function App (){
-    return <h1>Hello WorlD Brasil</h1>
+    return <RepositoryList />
 }
