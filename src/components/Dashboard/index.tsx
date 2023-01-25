7529e062cc153f2a85cff1ed213851b4a530965d
Wesley Isaias <wesley_isaias@hotmail.com>
2023-01-25 11:06:49 -0300

    Chapter II-02-Componetização OK

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,10 +1,12 @@
 import { Summary } from "../summary";
+import { TransactionsTable } from "../Transactions";
 import { Container } from "./styles";
 
 export function Dashboard(){
     return (
         <Container>
             <Summary/>
+            <TransactionsTable/>
 
         </Container>
     );
