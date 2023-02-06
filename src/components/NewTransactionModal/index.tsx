2cf3333d8cac7da1661fdf3e30bed08c19e88124
Wesley Isaias <wesley_isaias@hotmail.com>
2023-02-06 17:00:18 -0300

    Chapter II 04 Listando Transações

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -2,17 +2,37 @@ import Modal from 'react-modal';
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
 import closeImg from '../../assets/close.svg';
+import { api } from '../../services/api';
 import { Container, TransactionTypeContainer,RadioBox } from './styles';
-import { useState } from 'react';
+import { FormEvent, useState } from 'react';
+
 interface NewTransactionModalProps {
     isOpen: boolean;
     onRequestClose: () => void;
 }
 
 export function NewTransactionModal ({isOpen, onRequestClose}:NewTransactionModalProps){
+  const [title, setTitle] = useState ('');
+  const [value, setValue] = useState (0);
+  const [category, setCategory] = useState ('');
   const [type, setType] = useState('deposit');
 
-  
+  function handleCreateNewTransaction (event:FormEvent) {
+    event.preventDefault();
+
+    const data = {
+      title,
+      value,
+      category,
+      type
+    };
+
+    api.post ('/transactions', data)
+    
+
+  }
+
+
     return (
         <Modal
         isOpen= {isOpen} 
@@ -28,17 +48,23 @@ export function NewTransactionModal ({isOpen, onRequestClose}:NewTransactionModa
 
          </button>
  
-          <Container>
+          <Container onSubmit={handleCreateNewTransaction}>
           <h2>Cadastrar Transação</h2>
             <input placeholder="Título"
+            value={title}
+            onChange={event =>setTitle(event.target.value)}
             />
             
             <input
             type="number" 
             placeholder="Valor"
+            value={value}
+            onChange={event =>setValue (Number (event.target.value))}
             />
             
             <input placeholder="Categoria"
+            value={category}
+            onChange={event =>setCategory(event.target.value)}
             />
 
             <TransactionTypeContainer>
@@ -60,6 +86,7 @@ export function NewTransactionModal ({isOpen, onRequestClose}:NewTransactionModa
                 <img src= {outcomeImg} alt="Saída"/>
                   <span>Saída</span>
               </RadioBox>
+
 
             </TransactionTypeContainer>
 
